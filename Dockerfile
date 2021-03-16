7c77b947b2ce78f75e8d6b702af282fcda835516
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2021-03-16 13:32:24 -0700

    make app and dockerize

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM nginx
+
+# Copy a configuration file from the current directory
+#ADD nginx.conf /etc/nginx/
+#ADD mime.types /etc/nginx/
+
+ADD dist/ng-generator-cycles /usr/share/nginx/html/
+
+# Append "daemon off;" to the beginning of the configuration
+RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+
+# Expose ports
+EXPOSE 80
+
+# Set the default command to execute
+# when creating a new container
+CMD service nginx start
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,24 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+
+const numberIsValid = (control) => {
+  let num = parseFloat(control.value);
+  if (typeof num != 'number') {
+    return { num: "not number" };
+  }
+
+  if (Math.floor(num) != num) {
+    return { num: "not whole number" };
+  }
+  if (num <= 1) {
+    return { num: "less than one" }
+  }
+  return null;
+}
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +26,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-generator-cycles';
+  prime = new FormControl({value: 991, disabled: false}, [Validators.required, numberIsValid]);
+  primeInvalid = false;
+  generator = new FormControl({value: 7, disabled: false}, [Validators.required, numberIsValid]);
+  generatorInvalid = false;
+  current = null;
+  _begun = false;
+  cycleCount: number;
+  intervalId;
+  disapproval = false;
+
+  get begun() { return this._begun; }
+
+  set begun(condition: boolean) {
+    let command = condition ? 'disable' : 'enable';
+    this.prime[command]();
+    this._begun = condition;
+  }
+
+  start() {
+    if(this.generator.invalid || this.prime.invalid) {
+      this.begun = false;
+      return;
+    }
+
+    let generator = parseFloat(this.generator.value);
+    let prime = parseFloat(this.prime.value);
+
+    this.begun = true;
+    this.current = generator;
+    this.cycleCount = 1;
+    this.disapproval = false;
+    this.intervalId = setInterval(() => {
+      this.current = (this.current * generator) % prime;
+      this.cycleCount += 1;
+
+      if(this.current == generator || this.cycleCount > prime) {
+        this.begun = false;
+        clearInterval(this.intervalId);
+        if(this.cycleCount != prime) {
+          this.disapproval = true;
+        }
+      }
+    }, 800);
+
+
+  }
 }
